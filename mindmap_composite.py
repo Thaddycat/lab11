f07bb95580d964046b75c0ad5d7ca6eecdb26b9e
thaddycat <thaddeuslehmer@yahoo.com>
2024-11-19 20:41:03 -0800

    initial commit

---
+++ mindmap_composite.py
@@ -0,0 +1,31 @@
+import os
+
+class MindMapComposite:
+
+    def __init__(self, name, shape):
+        self.name = name
+        self.shape = shape
+        self.children = []
+
+    def add(self, child):
+        self.children.append(child)
+
+    def remove(self, child):
+        if child in self.children:
+            self.children.remove(child)
+        else:
+            print("Child not found in children list.")
+
+    def __str__(self):
+        shape_representation = self.get_shape_representation()
+        return shape_representation.format(self.name)
+
+    def display(self, indent=0):
+        result = " " * indent + str(self) + "\n"
+        for child in self.children:
+            result += child.display(indent + 2)
+        return result
+
+    def get_shape_representation(self):
+        shapes = {"circle": "(({}))", "oval": "({})", "square": "[{}]", "cloud": "){}(", "hexagon": "{{{{{}}}}}", "bang": ")){}(("}
+        return shapes.get(self.shape, "{}")
